2a7fc15b50a3f4ffc405a2d282cea63f405cf614
mohamedshabin009 <mohamedshabin009@gmail.com>
2024-10-26 14:53:44 +0530

    Create Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { LeaveModule } from './leave/leave.module';
+import { WfhModule } from './wfh/wfh.module';
+import { WorklogModule } from './worklog/worklog.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, LeaveModule, WfhModule, WorklogModule],
   controllers: [AppController],
   providers: [AppService],
 })
