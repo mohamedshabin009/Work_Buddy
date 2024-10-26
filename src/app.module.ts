e5e753d23e4450281b3eac1ff366abf614b4e002
mohamedshabin009 <mohamedshabin009@gmail.com>
2024-10-26 15:53:51 +0530

    Insert TypeOrm Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,26 @@ import { UserModule } from './user/user.module';
 import { LeaveModule } from './leave/leave.module';
 import { WfhModule } from './wfh/wfh.module';
 import { WorklogModule } from './worklog/worklog.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
-  imports: [UserModule, LeaveModule, WfhModule, WorklogModule],
+  imports: [
+    UserModule,
+    LeaveModule,
+    WfhModule,
+    WorklogModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root@123',
+      database: 'work_buddy',
+      entities: [join(__dirname, '**/*.entity{.ts,.js}')],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
