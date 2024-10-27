60d4383c1297a64a2a5c7304829935565779e353
mohamedshabin009 <mohamedshabin009@gmail.com>
2024-10-27 10:30:51 +0530

    Create TypeOrm

--- src/leave/leave.module.ts
+++ src/leave/leave.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Leave } from './leave.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Leave])],
+})
 export class LeaveModule {}
--- src/wfh/wfh.module.ts
+++ src/wfh/wfh.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Wfh } from './wfh.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Wfh])],
+})
 export class WfhModule {}
