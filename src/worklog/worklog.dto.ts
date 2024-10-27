fc8f6dfee876f20f66ecad9413a10b5d2275ea95
mohamedshabin009 <mohamedshabin009@gmail.com>
2024-10-27 10:11:25 +0530

    Create DTO

---
+++ src/worklog/worklog.dto.ts
@@ -0,0 +1,30 @@
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsDateString,
+  IsNotEmpty,
+  ValidateNested,
+} from 'class-validator';
+
+export class ClockEvent {
+  @IsNotEmpty()
+  @IsDateString()
+  clock_in_time: Date;
+
+  @IsNotEmpty()
+  @IsDateString()
+  clock_out_time: Date;
+}
+
+export class CreateWorkLogDto {
+  @IsNotEmpty()
+  @IsDateString()
+  date: Date;
+
+  @IsNotEmpty()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ClockEvent)
+  clockEvent: ClockEvent[];
+}
+export class UpdateWorkLogDto {}
