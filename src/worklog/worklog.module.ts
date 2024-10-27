f408fb9f78ae288a1c806c1fb6f910466b2544ed
mohamedshabin009 <mohamedshabin009@gmail.com>
2024-10-27 10:27:29 +0530

    Create TypeOrm

--- src/worklog/worklog.module.ts
+++ src/worklog/worklog.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WorkLog } from './worklog.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([WorkLog])],
+})
 export class WorklogModule {}
