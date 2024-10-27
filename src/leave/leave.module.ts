2215594fe23436542eddfae8d61ed61ad3dc1245
mohamedshabin009 <mohamedshabin009@gmail.com>
2024-10-27 21:32:14 +0530

    Added UserModule

--- src/leave/leave.module.ts
+++ src/leave/leave.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Leave } from './leave.entity';
+import { LeaveController } from './leave.controller';
+import { LeaveService } from './leave.service';
+import { UserService } from 'src/user/user.service';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Leave])],
+  imports: [TypeOrmModule.forFeature([Leave]), UserModule],
+  controllers: [LeaveController],
+  providers: [LeaveService],
 })
 export class LeaveModule {}
