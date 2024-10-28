6f3e7feb6d96777bb569ae97a80e08abc4736839
mohamedshabin009 <mohamedshabin009@gmail.com>
2024-10-28 12:19:59 +0530

    Clear the small Bug

--- src/worklog/worklog.entity.ts
+++ src/worklog/worklog.entity.ts
@@ -19,6 +19,6 @@ export class WorkLog {
   @Column()
   date: Date;
 
-  @Column('json', { array: true })
+  @Column({ type: 'json', nullable: true })
   clockEvent: { clock_in_time: Date; clock_out_time: Date }[];
 }
