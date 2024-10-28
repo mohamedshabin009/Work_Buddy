f0170eda4068b8284034d6ecf16b5b5ea4cc7e2f
mohamedshabin009 <mohamedshabin009@gmail.com>
2024-10-28 12:18:10 +0530

    Import UserModule

--- src/worklog/worklog.module.ts
+++ src/worklog/worklog.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WorkLog } from './worklog.entity';
+import { WorklogController } from './worklog.controller';
+import { WorklogService } from './worklog.service';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([WorkLog])],
+  imports: [TypeOrmModule.forFeature([WorkLog]), UserModule],
+  controllers: [WorklogController],
+  providers: [WorklogService],
 })
 export class WorklogModule {}
