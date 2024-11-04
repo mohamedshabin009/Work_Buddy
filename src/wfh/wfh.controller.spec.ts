5ce795792331d4daf445b0fba6dbb54b93d0060d
mohamedshabin009 <mohamedshabin009@gmail.com>
2024-11-04 16:47:25 +0530

    Testing

--- src/wfh/wfh.controller.spec.ts
+++ src/wfh/wfh.controller.spec.ts
@@ -1,18 +1,85 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WfhController } from './wfh.controller';
-
+import { WfhService } from './wfh.service';
+const mockService = {
+  createWorkFromHome: jest.fn(),
+  getAllWorkFromHome: jest.fn(),
+  getWorkFromHomeById: jest.fn(),
+  getWfhByUserId: jest.fn(),
+  updateWorkFromHome: jest.fn(),
+  deleteWorkFromHome: jest.fn(),
+};
 describe('WfhController', () => {
   let controller: WfhController;
+  let wfhService: WfhService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [WfhController],
+      providers: [
+        {
+          provide: WfhService,
+          useValue: mockService,
+        },
+      ],
     }).compile();
 
     controller = module.get<WfhController>(WfhController);
+    wfhService = module.get<WfhService>(WfhService);
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create new data', async () => {
+      const mockWfh = {
+        startDate: '2024-10-28',
+        endDate: '2024-11-30',
+        reason: 'Hamstring Injury',
+        status: 'Reject',
+        created_on: '00:47:17.459328',
+      } as any;
+      await controller.createWorkFromHome(mockWfh, 7);
+      expect(wfhService.createWorkFromHome).toHaveBeenCalledWith(mockWfh, 7);
+    });
+  });
+
+  describe('getAllWFH', () => {
+    it('should return all data', async () => {
+      await controller.getAllWorkFromHome();
+      expect(wfhService.getAllWorkFromHome).toHaveBeenCalledWith();
+    });
+  });
+  describe('getWFHById', () => {
+    it('should return specified data', async () => {
+      await controller.getWorkFromHomeById({ id: 2 });
+      expect(wfhService.getWorkFromHomeById).toHaveBeenCalledWith(2);
+    });
+  });
+  describe('getWfhByUserId', () => {
+    it('should return data with user', async () => {
+      await controller.getWfhByUserId({ userId: 2 });
+      expect(wfhService.getWfhByUserId).toHaveBeenCalledWith(2);
+    });
+  });
+  describe('update/WFH', () => {
+    it('should return Updated data', async () => {
+      const wfh = {
+        reason: 'Knee Pain, I can able to walk so..',
+      };
+      await controller.updateWorkFromHome(2, wfh);
+      expect(wfhService.updateWorkFromHome).toHaveBeenCalledWith(2, wfh);
+    });
+  });
+  describe('delete', () => {
+    it('should delete specified data', async () => {
+      await controller.deleteWorkFromHome(2);
+      expect(wfhService.deleteWorkFromHome).toHaveBeenCalledWith(2);
+    });
   });
 });
--- src/worklog/worklog.controller.spec.ts
+++ src/worklog/worklog.controller.spec.ts
@@ -1,9 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WorklogController } from './worklog.controller';
 import { WorklogService } from './worklog.service';
-import { WorkLog } from './worklog.entity';
-const mockservice = {
+const mockService = {
   createWorkLog: jest.fn(),
+  getAll: jest.fn(),
+  getWorkLogId: jest.fn(),
+  getWorkLogsByUserId: jest.fn(),
+  updateWorkLog: jest.fn(),
+  deleteWorkLog: jest.fn(),
 };
 describe('WorklogController', () => {
   let controller: WorklogController;
@@ -15,7 +19,7 @@ describe('WorklogController', () => {
       providers: [
         {
           provide: WorklogService,
-          useValue: mockservice,
+          useValue: mockService,
         },
       ],
     }).compile();
@@ -44,6 +48,50 @@ describe('WorklogController', () => {
       } as any;
       await controller.createWorkLog(mockWorklog, 7);
       expect(worklogService.createWorkLog).toHaveBeenCalledWith(mockWorklog, 7);
+    });
+  });
+  describe('getAllWorkLog', () => {
+    it('should return all work logs', async () => {
+      await controller.getAllWorkLog();
+      expect(worklogService.getAll).toHaveBeenCalledWith();
+    });
+  });
+
+  describe('getWorkLogById', () => {
+    it('should return workLog id', async () => {
+      const workLogId = 39;
+      await controller.getWorkLogById({ id: workLogId });
+      expect(worklogService.getWorkLogId).toHaveBeenCalledWith(workLogId);
+    });
+  });
+
+  describe('getWorkLogsByUserId', () => {
+    it('should return workLog Id with user Id', async () => {
+      const userId = 2;
+      await controller.getWorkLogsByUserId({ id: userId });
+      expect(worklogService.getWorkLogsByUserId).toHaveBeenCalledWith(userId);
+    });
+  });
+
+  describe('update', () => {
+    it('should return Updated value', async () => {
+      const updateUser = {
+        clockEvent: [
+          {
+            clock_in_time: '2024-10-28',
+            clock_out_time: '2024-10-28T18:30:00.000Z',
+          },
+        ],
+      } as any;
+      await controller.updateWorkLog(39, updateUser);
+      expect(worklogService.updateWorkLog).toHaveBeenCalledWith(39, updateUser);
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete data', async () => {
+      await controller.deleteWorkLog(39);
+      expect(worklogService.deleteWorkLog).toHaveBeenCalledWith(39);
     });
   });
 });
