9818896c56242344bf962ae6fd8ba28afe438e08
mohamedshabin009 <mohamedshabin009@gmail.com>
2024-11-05 16:16:04 +0530

    Testing

--- src/leave/leave.controller.spec.ts
+++ src/leave/leave.controller.spec.ts
@@ -1,18 +1,97 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LeaveController } from './leave.controller';
+import { LeaveService } from './leave.service';
+
+const mockService = {
+  createLeave: jest.fn(),
+  getAllLeave: jest.fn(),
+  getLeaveById: jest.fn(),
+  getLeaveRequestsByUserId: jest.fn(),
+  updateLeave: jest.fn(),
+  deleteLeave: jest.fn(),
+};
 
 describe('LeaveController', () => {
   let controller: LeaveController;
+  let leaveService: LeaveService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LeaveController],
+      providers: [
+        {
+          provide: LeaveService,
+          useValue: mockService,
+        },
+      ],
     }).compile();
 
     controller = module.get<LeaveController>(LeaveController);
+    leaveService = module.get<LeaveService>(LeaveService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('createLeave', () => {
+    it('should create new data', async () => {
+      const user = 9;
+      const mockService = {
+        startDate: '2024-10-28',
+        endDate: '2024-10-29',
+        reason: 'I should go to Passport office',
+        leaveType: 'CASUAL',
+        status: 'APPROVED',
+      } as any;
+      await controller.createLeave(mockService, user);
+    });
+  });
+  describe('getAllLeave', () => {
+    it('should return all leave', async () => {
+      await controller.getAllLeave();
+      expect(leaveService.getAllLeave).toHaveBeenCalledWith();
+    });
+  });
+  describe('getLeaveById', () => {
+    it('should return specified leave', async () => {
+      const leave = 33;
+      await controller.getLeaveById({ id: leave });
+      expect(leaveService.getLeaveById).toHaveBeenCalledWith(leave);
+    });
+  });
+  describe('getLeaveRequestsByUserId', () => {
+    it('should return leave with user', async () => {
+      const user = 10;
+      await controller.getLeaveRequestsByUserId({ userId: user });
+      expect(leaveService.getLeaveRequestsByUserId).toHaveBeenCalledWith(user);
+    });
+  });
+  describe('updateReq', () => {
+    it('should Update data', async () => {
+      const user = 34;
+      const updateLeave = {
+        startDate: '2024-10-11',
+
+        endDate: '2024-10-14',
+
+        reason: 'Viral Fever',
+
+        leaveType: 'SICK',
+      } as any;
+      await controller.updateLeave(user, updateLeave);
+      expect(leaveService.updateLeave).toHaveBeenCalledWith(user, updateLeave);
+    });
+  });
+  describe('delete', () => {
+    it('should delete the data', async () => {
+      const user = 10;
+      await controller.deleteLeave(user);
+      expect(leaveService.deleteLeave).toHaveBeenCalledWith(user);
+    });
   });
 });
--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,97 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+
+const mockService = {
+  createUser: jest.fn(),
+  getAll: jest.fn(),
+  getById: jest.fn(),
+  updateUser: jest.fn(),
+  deleteUser: jest.fn(),
+  searchUserName: jest.fn(),
+};
 
 describe('UserController', () => {
   let controller: UserController;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: mockService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('register', () => {
+    it('should create new data', async () => {
+      const user = {
+        name: 'marcelo',
+        password: 'mar@123',
+        role: 'EMPLOYEE',
+        email: 'celo@gmail.com',
+        mobileNumber: '+919876204440',
+        profileImage: 'm6.png',
+      } as any;
+      await controller.createUser(user);
+      expect(userService.createUser).toHaveBeenCalledWith(user);
+    });
+  });
+
+  describe('getAllUser', () => {
+    it('should return all data', async () => {
+      await controller.getAllUsers();
+      expect(userService.getAll).toHaveBeenCalledWith();
+    });
+  });
+
+  describe('getUserById', () => {
+    it('should return specified data', async () => {
+      const user = 10;
+      await controller.getUserById({ id: user });
+      expect(userService.getById).toHaveBeenCalledWith(user);
+    });
+  });
+
+  describe('updateUser', () => {
+    it('should update the data', async () => {
+      const user = 10;
+      const updateUser = {
+        mobileNumber: '+919786001100',
+      };
+      await controller.updateUser(updateUser, user);
+      expect(userService.updateUser).toHaveBeenCalledWith(user, updateUser);
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete the data', async () => {
+      const user = 10;
+      await controller.deleteUser(user);
+      expect(userService.deleteUser).toHaveBeenCalledWith(user);
+    });
+  });
+
+  describe('search/user', () => {
+    it('should return searched data', async () => {
+      const userName = 'ka';
+      await controller.searchUser({ name: userName });
+      expect(userService.searchUserName).toHaveBeenCalledWith(userName);
+    });
   });
 });
